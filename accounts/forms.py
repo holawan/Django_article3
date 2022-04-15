2f9894fbcf703c6717eea5dfb075805deb4be298
holawan <asdf134652@gmail.com>
2022-04-15 16:38:29 +0900

    user_update

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm,UserChangeForm
 from django.contrib.auth import get_user_model
 
 class CustomUserCreationForm(UserCreationForm):
@@ -7,3 +7,10 @@ class CustomUserCreationForm(UserCreationForm):
         model = get_user_model()
         fields = UserCreationForm.Meta.fields
 
+
+class CustomUserChangeForm(UserChangeForm) :
+
+    class Meta :
+        model = get_user_model()
+
+        fields= ('first_name','last_name',)
--- accounts/views.py
+++ accounts/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import login as auth_login
 from django.contrib.auth import logout as auth_logout
 from django.contrib.auth.forms import AuthenticationForm, PasswordChangeForm
-from .forms import CustomUserCreationForm
+from .forms import CustomUserChangeForm, CustomUserCreationForm
 # Create your views here.
 
 def login(request):
@@ -40,3 +40,17 @@ def signup(request):
         'form':form
     }
     return render(request, 'accounts/signup.html', context)
+
+def update(request):
+    if request.method=='POST':
+        form = CustomUserChangeForm(request.POST,instance=request.user)
+        if form.is_valid():
+            user = form.save()
+            auth_login(request, user)
+            return redirect('movies:index')
+    else:
+        form = CustomUserChangeForm(instance=request.user)
+    context = {
+        'form':form
+    }
+    return render(request, 'accounts/update.html', context)
