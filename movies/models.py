56a004e6171285db446cf243918b204031fc682d
holawan <asdf134652@gmail.com>
2022-04-15 13:21:26 +0900

    index.html

--- movies/models.py
+++ movies/models.py
@@ -10,8 +10,6 @@ class Movie(models.Model) :
 
 
 class Comment(models.Model) :
-
-
     user = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE)
     movie = models.ForeignKey(Movie,on_delete=models.CASCADE)
     content = models.CharField(max_length=100)
