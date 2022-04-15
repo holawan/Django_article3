2dd755562f675d3ae483531031717810b409ddbe
1yonghyun <zyonghyunz@gmail.com>
2022-04-15 16:13:16 +0900

    sign up

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,42 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib.auth import login as auth_login
+from django.contrib.auth import logout as auth_logout
+from django.contrib.auth.forms import AuthenticationForm, PasswordChangeForm
+from .forms import CustomUserCreationForm
 # Create your views here.
+
+def login(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            auth_login(request, user)
+            return redirect('movies:index')
+    else:
+        form = AuthenticationForm()
+    context = {
+        'form' : form
+    }
+
+    return render(request, 'accounts/login.html', context)
+
+
+def logout(request):
+    if request.method == 'POST':
+        if request.user.is_authenticated:
+            auth_logout(request)
+    return redirect('movies:index')
+
+def signup(request):
+    if request.method=='POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            auth_login(request, user)
+            return redirect('movies:index')
+    else:
+        form = CustomUserCreationForm()
+    context = {
+        'form':form
+    }
+    return render(request, 'accounts/signup.html', context)
