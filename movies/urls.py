2077c82a2df39ef42fcfde8d77e40be0f4bdae2f
1yonghyun <zyonghyunz@gmail.com>
2022-04-15 14:05:53 +0900

    create

--- movies/urls.py
+++ movies/urls.py
@@ -6,4 +6,5 @@ app_name='movies'
 
 urlpatterns = [
     path('',views.index,name='index'),
+    path('create/',views.create,name='create'),
 ]
--- movies/views.py
+++ movies/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from movies.forms import MovieForm
 from .models import Movie
 # Create your views here.
 
@@ -13,4 +15,16 @@ def index(request) :
 
     return render(request,'movies/index.html',context)
 
+def create(request):
+    if request.method=='POST':
+        form = MovieForm(request.POST)
+        if form.is_valid():
+            movie = form.save()
+            movie.save()
+            return redirect('movies:index')
+    form = MovieForm()
+    context = {
+        'form' : form
+    }
+    return render(request,'movies/create.html', context)
 
